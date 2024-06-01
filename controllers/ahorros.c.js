a9a1f1e501ae6765dc105cc3abfc9236d591f521
anda2210 <angeldavidcastellano1@gmail>
2024-06-01 13:00:07 -0400

    Quinta actualizacion del proyecto

---
+++ controllers/ahorros.c.js
@@ -0,0 +1,64 @@
+const { ahorros, usuarios } = require("../database/database");
+const { v4: uuidv4 } = require('uuid');
+
+class ahorrosControllers {
+    listar() {
+        return new Promise((resolve, reject) => {
+            try {
+                if (ahorros.length === 0) {
+                    return reject('No hay cuentas de ahorros registradas en el Banco')
+                }
+                const data = []
+                ahorros.forEach(ahorro => {
+                    data.push({
+                        balance: ahorro.balance + "Bs",
+                        intereses: ahorro.intereses + "%",
+                        dueño: ahorro.dueño,
+                        numero_cuenta: ahorro.numero_cuenta
+                    })
+                });
+                return resolve({
+                    data: data,
+                    mensaje: "Listado con éxito las cuentas de ahorros"
+                })
+            } catch (error) {
+                return reject(error);
+            }
+        });
+    };
+
+    agregar(ahorro) {
+        return new Promise((resolve, reject) => {
+            try {
+                if (!ahorro.dueño) {
+                    return reject("Faltan propiedades escenciales para agregar la cuenta")
+                }
+                for (let i = 0; i < usuarios.length; i++) {
+                    if (usuarios[i].usuario === ahorro.dueño) {
+                        let cuenta_nueva = {
+                            balance: 0,
+                            intereses: 5,
+                            dueño: ahorro.dueño,
+                            numero_cuenta: uuidv4()
+                        }
+                        ahorros.push(cuenta_nueva);
+                        return resolve({
+                            mensaje: "Se ha agregado con éxito la cuenta de ahorro al usuario " + ahorro.dueño,
+                            data: {
+                                balance: cuenta_nueva.balance + "Bs",
+                                intereses: cuenta_nueva.intereses + "%",
+                                dueño: cuenta_nueva.dueño,
+                                numero_cuenta: cuenta_nueva.numero_cuenta
+                            }
+                        })
+                    }
+                }
+                return reject("No existe el usuario que será dueño de la cuenta")
+            } catch (error) {
+                return reject(error);
+            }
+        });
+    }
+}
+
+module.exports = new ahorrosControllers();
---
+++ routes/ahorros.js
@@ -0,0 +1,41 @@
+var express = require('express');
+const ahorrosC = require('../controllers/ahorros.c');
+var router = express.Router();
+
+/* GET Cuentas ahorros: */
+router.get('/', function(req, res, next) {
+  ahorrosC.listar()
+  .then((respuesta) => {
+    res.status(200).json({
+      status: "200",
+      mensaje: respuesta.mensaje,
+      cuentas_ahorro: respuesta.data
+    })
+  })
+  .catch((error) => {
+    res.status(400).json({
+      status: "400",
+      mensaje: error
+    })
+  })
+});
+
+/* POST Cuentas ahorros: */
+router.post('/agregar', function(req, res, next) {
+    ahorrosC.agregar(req.body)
+    .then((respuesta) => {
+      res.status(201).json({
+        status: "201",
+        mensaje: respuesta.mensaje,
+        cuenta_ahorro: respuesta.data
+      })
+    })
+    .catch((error) => {
+      res.status(400).json({
+        status: "400",
+        mensaje: error
+      })
+    })
+});
+
+module.exports = router;
